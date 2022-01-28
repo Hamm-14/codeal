1addb68105ffd1f48d6fd334dc6e430f68b63458
Hammad <hammadyasub14@gmail.com>
2022-01-28 12:08:48 +0530

    create delete comment from a post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,22 @@ module.exports.create = function(req,res){
         return res.redirect('back');
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.query.cid,function(err,comment){
+        if(err){console.log("Error in fetching the comment from db");return;}
+        
+        if(comment.user == req.user.id){
+            comment.remove();
+
+            Post.findById(req.query.pid,function(err,post){
+                if(err){console.log("Error in finding the post in db");return;}
+
+                let index = post.comments.indexOf(req.query.cid);
+                post.comments.splice(index,1);
+                post.save();
+            });
+        }
+        return res.redirect('back');
+    });
+}
