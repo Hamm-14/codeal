3909f0087958dc14ac13192e05f532678683c0d7
Hammad <hammadyasub14@gmail.com>
2022-02-24 20:56:22 +0530

    create and join chatroom and send/receive message

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,3 +1,5 @@
+
+
 class chatEngine{
     constructor(chatBoxId,userEmail){
         this.chatBox = $(`#${chatBoxId}`);
@@ -10,9 +12,56 @@ class chatEngine{
     }
 
     connectionHandler(){
-        console.log('inside connection handler');
+        let self = this;
+
         this.socket.on('connect',function(){
             console.log('connection established using sockets....!');
+
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log('a user joined',data);
+            });
+
+            $('#send-message').click(function(){
+                let msg = $('#chat-message-input').val();
+
+                if(msg != ''){
+                    self.socket.emit('send_message',{
+                        message: msg,
+                        user_email: self.userEmail,
+                        chatroom: 'codeial'
+                    });
+                }
+            });
+
+            self.socket.on('receive_message',function(data){
+                let newMessage= $('<li>');
+
+                let messageType = 'other-message';
+                if(data.user_email == self.userEmail){
+                    messageType = 'self-message';
+                }
+
+                newMessage.append($('<span>',{
+                    'html': data.message
+                }));
+
+                newMessage.append($('<sub>',{
+                    'html': data.user_email
+                }));
+
+                newMessage.addClass(messageType);
+
+                $('#chat-message-list').append(newMessage);
+
+                var objDiv = $("#chat-message-list");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            });
         });
+
     }
 }
